4e193df865113ed5e1c23805241bf771f69e54ba
Anirban Sarkar <anirbansarkar.slg18@gmail.com>
2026-01-10 16:51:14 +0530

    this is a git commit message template

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,12 +4,12 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 
 const AppRoutes: React.FC = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    );
 };
 
-            export default AppRoutes;
+export default AppRoutes;
